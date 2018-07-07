4fa821ca72956f9e7b5dfad0c82233cfe9307457
Rafael Mercedes <rafaelmersant@gmail.com>
2018-07-06 21:10:47 -0400

    Update design for Candidates, and change bootstrap to standard.

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace SistemaElecciones
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-sandstone.css",
+                      "~/Content/bootstrap.css",
                       "~/Content/Site.css"));
         }
     }
